1bbb296762789a8835a40b83e12473bacff76839
André Matias <matiasandreyt@gmail.com>
2025-11-22 09:13:31 +0000

    {login + jwt + front - end (login) } Done , However, it still has a design problem on the login, a grey div appears and when you inspect it, it disappears.

--- src/main/java/dev/matias/springsecurity/config/SecurityConfig.java
+++ src/main/java/dev/matias/springsecurity/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/users").permitAll()
                 .requestMatchers(HttpMethod.POST, "/login").permitAll()
                 .requestMatchers(HttpMethod.GET, "/login-page").permitAll()
+                    .requestMatchers(HttpMethod.PATCH, "/tweets").permitAll()
                 // .requestMatchers(HttpMethod.GET, "/home").permitAll()
                 .requestMatchers("/home").authenticated()
                 .requestMatchers(HttpMethod.GET, "/me").permitAll()
